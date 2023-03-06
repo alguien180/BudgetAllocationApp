7466d40f206a927e9dcc5be6c0923f4cd841b798
alguien180 <miguel_agj@protonmail.com>
2023-03-06 11:53:04 +0100

    Changed design, class eliminated. Changed to HashMap instead of array list. Missing exception handling

--- BudgetAllocatorApp.java
+++ BudgetAllocatorApp.java
@@ -1,19 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 public class BudgetAllocatorApp {
     public static void main(String[] args) {
-        Database actualDatabase = new Database();
 
 
-        System.out.println("Welcome, what would you like to do?\n 1. Input a new expense \n 2. Re-allocate budget to another section \n 3. See the current budget allocation\n 4. exit programm");
         Scanner sc1 = new Scanner(System.in);
+
+        ArrayList<String> nameOfExpense = new ArrayList<>();
+        ArrayList<Double> priceList = new ArrayList<>();
+
+        Map<String, Double> ExpenseListMixed = new HashMap<>();
+        System.out.println("Please enter your total budget");
+        double totalMoney = sc1.nextDouble();
+
+        System.out.println("Welcome, what would you like to do?\n 1. Input a new expense \n 2. Re-allocate budget to another section \n 3. See the current budget allocation \n 4. re-set total budget \n 5. exit programm");
         int entry = 0;
         entry = sc1.nextInt();
-        while (entry !=4){
+     while (entry !=5){
         /*
         try{
             System.out.println("entered the try");
             entry = sc1.nextInt();
-
         }catch(Exception e){
             System.out.println("entered the catch");
             while (!sc1.hasNextInt()){
@@ -21,24 +30,37 @@ public class BudgetAllocatorApp {
                 System.out.println("invalid input, re-enter the option");
                 entry = sc1.nextInt();
             }
-        }
+        }*/
         while(entry <0 || entry >4 ){
             System.out.println("invalid input, re-enter the option");
             entry = sc1.nextInt();
-        }*/
+        }
         if (entry == 1) {
-            actualDatabase.addExpense();
-            System.out.println("\n \n What would you like to do now?\n 1. Input a new expense \n 2. Re-allocate budget to another section \n 3. See the current budget allocation\n 4. exit programm");
+            System.out.println("enter object name");
+            String name=sc1.next();
+
+
+
+            System.out.println("enter object cost");
+            double  cost=sc1.nextDouble();
+            ExpenseListMixed.put(name,cost);
+            double moneyAllocated = 0;
+            for(Double val : ExpenseListMixed.values()){
+                moneyAllocated += val;}
+            System.out.println("Entered"+ ExpenseListMixed+ ","+ (totalMoney-moneyAllocated) + " remaining from your total budget");
+            System.out.println("\n\nWhat would you like to do?\n 1. Input a new expense \n 2. Re-allocate budget to another section \n 3. See the current budget allocation \n 4. re-set total budget \n 5. exit programm");
             entry = sc1.nextInt();
         }else if (entry ==2){
             System.out.println("pate");
-            System.out.println("\n \n What would you like to do now?\n 1. Input a new expense \n 2. Re-allocate budget to another section \n 3. See the current budget allocation\n 4. exit programm");
+            System.out.println("\n\nWhat would you like to do?\n 1. Input a new expense \n 2. Re-allocate budget to another section \n 3. See the current budget allocation \n 4. re-set total budget \n 5. exit programm");
             entry = sc1.nextInt();
         }else if (entry ==3 ){
-            System.out.println(actualDatabase.toString());
-            System.out.println("\n \n What would you like to do now?\n 1. Input a new expense \n 2. Re-allocate budget to another section \n 3. See the current budget allocation\n 4. exit programm");
+            System.out.println(ExpenseListMixed);
+            System.out.println("\n\nWhat would you like to do?\n 1. Input a new expense \n 2. Re-allocate budget to another section \n 3. See the current budget allocation \n 4. re-set total budget \n 5. exit programm");
             entry = sc1.nextInt();
-
+        } else if(entry ==4){
+            System.out.println("\n\nWhat would you like to do?\n 1. Input a new expense \n 2. Re-allocate budget to another section \n 3. See the current budget allocation \n 4. re-set total budget \n 5. exit programm");
+            totalMoney = sc1.nextDouble();
         }
     }
     }
